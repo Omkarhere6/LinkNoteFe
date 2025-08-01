0a9b934d4437f2c8eb2e420dadd0fdc39bcce7e9
Omkar Hawaldar <o.hawaldar@bm-systems.in>
2025-08-01 17:48:19 +0530

    Ui improvements

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,16 +1,44 @@
+import axios from "axios";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import {useRef} from "react"
+import { BACKEND_URL } from "../config";
+import { useNavigate } from "react-router-dom";
 
 export function SignIn(){
+    const userNameReference = useRef<HTMLInputElement>(null);
+    const passwordRefrence  = useRef<HTMLInputElement>(null);
+    const navigate = useNavigate();
+    const signIn =async () =>{
+        const userName = userNameReference.current?.value;
+        const password = passwordRefrence.current?.value;
+        await axios.post(`${BACKEND_URL}/api/v1/signin`,{
+                    username : userName,
+                    password : password
+            }).then((response)=>{
+                alert(response.data.message)
+                navigate("/dashboard")
+            }).catch((error)=>{
+                alert(error)
+            })
+
+    }
+
     return (
         <div className="h-screen w-full bg-gray-200 flex justify-center items-center">
             <div className="bg-white rounded-xl border min-w-48 p-8">
                 <div className="space-y-4">
-                    <Input type="text" placeholder="Enter user name"/>
-                    <Input type="password" placeholder="Enter password"/>
-                    <div className="flex justify-center pt-4">
-                        <Button variant="Primary" text="Sign In" loading={false}></Button>    
+                    <div className="flex justify-center">
+                        <h1 className="items-center text-gray-600 text-2xl">Sign In</h1>
                     </div>
+                    <Input ref={userNameReference} type="text" placeholder="Enter user name"/>
+                    <Input ref={passwordRefrence} type="password" placeholder="Enter password"/>
+                    <div className="flex justify-center pt-4">
+                        <Button onClick={signIn} variant="Primary" text="Sign In" loading={false}></Button>    
+                    </div>
+                    <a onClick={() =>{
+                        navigate("/")
+                    }}>Don't have a account ? Sign up</a>
                 </div>
             </div>
         </div>
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -29,11 +29,17 @@ export function SignUp(){
         <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
             <div className="bg-white rounded-xl border min-w-48 p-8">
                 <div className="space-y-4">
+                <div className="flex justify-center">
+                        <h1 className="items-center text-gray-600 text-2xl">Sign Up</h1>
+                    </div>
                     <Input type="text" placeholder="Enter user name" ref={userNameRef}/>
                     <Input type="password" placeholder="Enter password" ref={passwordRef}/>
                     <div className="flex justify-center pt-4">
                         <Button variant="Primary" text="Sign Up" loading={false} onClick={signUp}></Button>    
                     </div>
+                    <a onClick={() =>{
+                        navigate("/signin")
+                    }}>Already user ? Sign in</a>
                 </div>
             </div>
         </div>
